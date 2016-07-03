a55a9d2e5da8754d2086b97814aa73f01576856d
rmcfadden <ryanpmcfadden@gmail.com>
2016-07-03 15:17:38 -0700

    Added bootstrap and sequelize.  Started mysql setup and test migratoins.

---
+++ migrations/20160703210555-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      bio: {
+        type: Sequelize.TEXT
+      },
+      created: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  }
+};
