01c5708e09589065d77bee98c82eb3fdfad1edf6
yogesh-imorsetech <yogesh@imorsetech.com>
2016-07-25 14:52:58 +0530

    adding delete user case

--- test/unit/user.js
+++ test/unit/user.js
@@ -31,13 +31,41 @@ describe('create user', function () {
 describe('update user', function () {
   it('should update a email id', function (done) {
     var email = 'test' + randomstring.generate() + "@test.com";
-    console.log(email);
     models.user.update(
       { email: email }, /* set attributes' value */
       { where: { username: 'johnNtOPTZR9EKn12JQs5lwYQTzqTWMEjj9s' }} /* where criteria */
     ).then(function(user) {    
       done();
     })
+  });
+});
+
+
+describe('delete user', function () {
+  it('should delete a user', function (done) {
+    var email = randomstring.generate() + "@abc.com";
+    var username = "Tim" + randomstring.generate();
+    models.user.create({
+      username: username,
+      email: email,
+      password: "secret",
+      password_salt: "123"
+    }).then(function(user) {
+      user.id.should.be.greaterThan(0);
+      user.username.should.equal(username);
+      user.email.should.equal(email);
+      user.password.should.equal("secret");
+      user.password_salt.should.equal("123");
+
+      user.created.should.be.greaterThan(0);
+      user.updated.should.be.greaterThan(0);
+      
+      models.user.destroy({ where : { id : user.id} })
+      .then(function(status) {
+	done();
+      });
+      done();
+    });    
   });
 });
 
