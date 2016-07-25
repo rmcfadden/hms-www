6232c792799b81a64e06a9f8bce8179dd3e7e2b6
yogesh-imorsetech <yogesh@imorsetech.com>
2016-07-25 16:35:48 +0530

    updated test cases for update and delete scenarios

--- test/unit/user.js
+++ test/unit/user.js
@@ -3,6 +3,7 @@
 var models  = require('../../models');
 var should  = require('should');
 var randomstring = require("randomstring");
+var async = require('async');
 
 describe('create user', function () {
   it('should return a valid user', function (done) {
@@ -30,44 +31,91 @@ describe('create user', function () {
 
 describe('update user', function () {
   it('should update a email id', function (done) {
-    var email = 'test' + randomstring.generate() + "@test.com";
-    models.user.update(
-      { email: email }, /* set attributes' value */
-      { where: { username: 'johnNtOPTZR9EKn12JQs5lwYQTzqTWMEjj9s' }} /* where criteria */
-    ).then(function(user) {    
-      done();
-    })
-  });
-});
+    async.waterfall([
+      function createUser(next){
+        var email = randomstring.generate() + "@test.com";
+        var username = "john" + randomstring.generate();
 
+        models.user.create({ username: username,
+          email: email,
+          password: "secret",
+          password_salt: "123"
+        }).then(function(user){
+          user.id.should.be.greaterThan(0);
+          user.username.should.equal(username);
+          user.email.should.equal(email);
+          user.password.should.equal("secret");
+          user.password_salt.should.equal("123");
 
-describe('delete user', function () {
-  it('should delete a user', function (done) {
-    var email = randomstring.generate() + "@abc.com";
-    var username = "Tim" + randomstring.generate();
-    models.user.create({
-      username: username,
-      email: email,
-      password: "secret",
-      password_salt: "123"
-    }).then(function(user) {
-      user.id.should.be.greaterThan(0);
-      user.username.should.equal(username);
-      user.email.should.equal(email);
-      user.password.should.equal("secret");
-      user.password_salt.should.equal("123");
+          user.created.should.be.greaterThan(0);
+          user.updated.should.be.greaterThan(0); 
+          next(null, username);
+        });
+      },
+      function updateUser(username, next){
+        var email = 'test' + randomstring.generate() + "@test.com";
 
-      user.created.should.be.greaterThan(0);
-      user.updated.should.be.greaterThan(0);
-      
-      models.user.destroy({ where : { id : user.id} })
-      .then(function(status) {
+        models.user.update(
+          { email: email }, /* set attributes' value */
+          { where: { username: username }} /* where criteria */
+        ).then(function(user) {   
+          next(null, username, email);
+        });
+      },
+      function findUser(username, email, next){
+	models.user.find({where : { email : email }}).then(function(user){
+	  user.id.should.be.greaterThan(0);
+          user.username.should.equal(username);
+          user.email.should.equal(email);
+          user.password.should.equal("secret");
+          user.password_salt.should.equal("123");
 
-      });
+          user.created.should.be.greaterThan(0);
+          user.updated.should.be.greaterThan(0);
+          next();
+        });
+      }
+    ],function(error, result){
       done();
     });    
   });
 });
 
 
+describe('delete user', function () {
+  it('should delete a user', function (done) {
+     async.waterfall([
+       function createUser(next){
+         var email = randomstring.generate() + "@abc.com";
+         var username = "Tim" + randomstring.generate();
+	 models.user.create({
+	   username: username,
+	   email: email,
+	   password: "secret",
+	   password_salt: "123"
+	 }).then(function(user) {
+	   user.id.should.be.greaterThan(0);
+	   user.username.should.equal(username);
+	   user.email.should.equal(email);
+	   user.password.should.equal("secret");
+	   user.password_salt.should.equal("123");
+           user.created.should.be.greaterThan(0);
+	   user.updated.should.be.greaterThan(0);
+	   next(null, user.id);
+	 });
+       },
+       function deleteUser(user_id){
+         models.user.destroy({ where : { id : user_id} })
+          .then(function(status) {
+	    models.user.findAll({where : { id : user_id }}).then(function(users){
+              users.length.should.equal(0);
+	      done();
+	    });
+        });
+      }
+    ],function(error, result){
+     done();
+    });    
+  });
+});
 
