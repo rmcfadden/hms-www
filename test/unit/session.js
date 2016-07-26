25bf7557b703600424a6c2e310748756913884b7
yogesh-imorsetech <yogesh@imorsetech.com>
2016-07-26 15:09:54 +0530

    adding findall and findone cases

--- test/unit/session.js
+++ test/unit/session.js
@@ -216,3 +216,83 @@ describe('delete session', function () {
   });
 });
 
+describe('Find all sessions', function () {
+  it('should return a valid list of sessions', function (done) {
+    async.waterfall([
+       function createUser(next){
+         var email = randomstring.generate() + "@abc.com";
+         var username = "Tim" + randomstring.generate();
+	 models.user.create({
+	   username: username,
+	   email: email,
+	   password: "secret",
+	   password_salt: "123"
+	 }).then(function(user) {
+	   user.id.should.be.greaterThan(0);
+	   user.username.should.equal(username);
+	   user.email.should.equal(email);
+	   user.password.should.equal("secret");
+	   user.password_salt.should.equal("123");
+           user.created.should.be.greaterThan(0);
+	   user.updated.should.be.greaterThan(0);
+	   next(null, user.id);
+	 });
+       },
+       function createSession(user_id, next){
+
+	 var token = 'List'+randomstring.generate();
+	 var now = moment();
+	 var lastActivity = now.subtract(8, 'h');
+	 var end = now.add(1, 'd');
+	 models.session.create({
+	   user_id: user_id,
+	   token: token,
+	   start: now,
+           end: end,
+	   last_activity_date: lastActivity,
+	   hit_count: 1,
+           is_expired: 0
+	 }).then(function(session) {
+	   session.token.should.equal(token);
+	   session.user_id.should.equal(user_id);
+	   session.id.should.be.greaterThan(0);
+	   session.start.should.be.greaterThan(0);
+	   session.end.should.be.greaterThan(0);
+	   session.last_activity_date.should.be.greaterThan(0);
+	   session.is_expired.should.equal(false);
+  	   session.created.should.be.greaterThan(0);
+	   session.updated.should.be.greaterThan(0);
+	   next(null);
+	 });
+      },
+      function findAll(){
+	console.log('HERE 269');
+	models.session.findAll().then(function(sessions) {
+          sessions.length.should.be.greaterThan(0);
+          done();
+	});
+      }
+    ],function(error, result){
+     done();
+    });    
+  });
+});
+
+
+describe('find session with id 1', function () {
+it('should return a valid session record', function (done) {
+  models.session.findOne({ where: { id: 1 }
+    }).then(function(session) {
+      session.id.should.be.greaterThan(0);
+      session.user_id.should.be.greaterThan(0);
+      session.start.should.be.greaterThan(0);
+      session.end.should.be.greaterThan(0);
+      session.last_activity_date.should.be.greaterThan(0);
+      session.hit_count.should.be.greaterThan(0);
+      session.created.should.be.greaterThan(0);
+      session.updated.should.be.greaterThan(0);
+
+      done();
+    });
+  });
+});
