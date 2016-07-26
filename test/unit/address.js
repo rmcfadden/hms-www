43fa0c39d1f6d1e486bf9da8555555adfbc04871
yogesh-imorsetech <yogesh@imorsetech.com>
2016-07-26 17:09:26 +0530

    delete address case

--- test/unit/address.js
+++ test/unit/address.js
@@ -100,3 +100,49 @@ describe('update address', function () {
   });
 });
 
+
+describe('delete address', function () {
+  it('should delete a address', function (done) {
+     async.waterfall([
+       function createAddress(next){
+        models.country.find({where : { iso_code2 : 'US' }}).then(function(country){
+	      models.address.create({
+		address_line1: 'testing address1',
+		address_line2: 'testing address2',
+		city: 'Santa Barbara',
+		province: 'California',
+		postal_code: '93105',
+		country_id: country.id
+	      }).then(function(address) {
+
+		address.id.should.be.greaterThan(0);
+		address.uuid.should.not.be.null();
+		address.address_line1.should.be.equal('testing address1');
+		address.address_line2.should.be.equal('testing address2');
+		address.city.should.be.equal('Santa Barbara');
+		address.province.should.be.equal('California');
+		address.postal_code.should.be.equal('93105');
+		address.country_id.should.be.equal(country.id);
+		address.created.should.be.greaterThan(0);
+		address.updated.should.be.greaterThan(0);
+
+		next(null, address.id);
+	      });
+	    });
+      },
+       function deleteAddress(address_id){
+         models.address.destroy({ where : { id : address_id} })
+          .then(function(status) {
+	    models.address.findAll({where : { id : address_id }}).then(function(addresses){
+              addresses.length.should.equal(0);
+	      done();
+	    });
+        });
+      }
+    ],function(error, result){
+     done();
+    });    
+  });
+});
+
+
