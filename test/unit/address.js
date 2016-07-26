ed3f5d6643e3117c6a8415a4b25776ed862cd95b
yogesh-imorsetech <yogesh@imorsetech.com>
2016-07-26 17:14:46 +0530

    find all and fine one address case

--- test/unit/address.js
+++ test/unit/address.js
@@ -145,4 +145,55 @@ describe('delete address', function () {
   });
 });
 
+describe('find all', function () {
+  before(function(){
+    models.country.find({where : { iso_code2 : 'US' }}).then(function(country){
+      models.address.create({
+        address_line1: 'testing address1',
+        address_line2: 'testing address2',
+        city: 'Santa Barbara',
+        province: 'California',
+        postal_code: '93105',
+        country_id: country.id
+      }).then(function(address) {
+
+        address.id.should.be.greaterThan(0);
+        address.uuid.should.not.be.null();
+        address.address_line1.should.be.equal('testing address1');
+        address.address_line2.should.be.equal('testing address2');
+        address.city.should.be.equal('Santa Barbara');
+        address.province.should.be.equal('California');
+        address.postal_code.should.be.equal('93105');
+        address.country_id.should.be.equal(country.id);
+        address.created.should.be.greaterThan(0);
+        address.updated.should.be.greaterThan(0);
+
+        address.destroy().then(function(result){          
+          result.id.should.be.greaterThan(0);
+          done();
+        });
+      });
+    });
+  })
+  it('should return a valid list of address', function (done) {
+    models.address.findAll().then(function(addresses) {
+      addresses.length.should.be.greaterThan(0);
+      done();
+    });
+  });
+});
+
+
+describe('find address with id 1', function () {
+it('should return a valid address', function (done) {
+  models.address.findOne({ where: { id: 1 }
+    }).then(function(address) {
+      address.id.should.be.greaterThan(0);
+      address.created.should.be.greaterThan(0);
+      address.updated.should.be.greaterThan(0);
+
+      done();
+    });
+  });
+});
 
