7d7e3826bb4ebb08aa149637f731a0638bb69561
rmcfadden <ryanpmcfadden@gmail.com>
2016-07-26 21:08:07 -0700

    Started routes and ejs to destination page

--- routes/destination.js
+++ routes/destination.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/destination', function(req, res, next) {
+router.get('/destination/:iso_code2/:name', function(req, res, next) {
   res.render('destination', { title: 'Express' });
 });
 
