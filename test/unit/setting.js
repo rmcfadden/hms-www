6c98b2c157b24656ad133f9e62af4561f8be6fa8
yogesh-imorsetech <yogesh@imorsetech.com>
2016-07-27 12:54:05 +0530

    adding delete case for settings

--- test/unit/setting.js
+++ test/unit/setting.js
@@ -47,57 +47,105 @@ describe('Update settings', function () {
   it('should update a settings data', function (done) {
      async.waterfall([
        function createSettingCategory(next){
-         var name = randomstring.generate() + "-setting";
-			models.setting_categories.create({      
-			  name: name
-			}).then(function(setting_categories) {
-			  setting_categories.id.should.be.greaterThan(0);
-			  setting_categories.name.should.equal(name);
-			  setting_categories.created.should.be.greaterThan(0);
-			  setting_categories.updated.should.be.greaterThan(0);
-			  next(null, setting_categories.id);
-			});  
+          var name = randomstring.generate() + "-setting";
+          models.setting_categories.create({      
+            name: name
+          }).then(function(setting_categories) {
+            setting_categories.id.should.be.greaterThan(0);
+            setting_categories.name.should.equal(name);
+            setting_categories.created.should.be.greaterThan(0);
+            setting_categories.updated.should.be.greaterThan(0);
+            next(null, setting_categories.id);
+          });  
        },
        function createSetting(category_id, next){
-		 var name = "Setting " + randomstring.generate({ length: 3, charset: 'numeric' });
-		 var value = randomstring.generate();
-         models.settings.create({
-		   setting_category_id: category_id,
-		   name: name,
-		   value: value
-		 }).then(function(settings) {
-		   settings.id.should.be.greaterThan(0);
-		   settings.setting_category_id.should.equal(category_id);
-		   settings.name.should.be.equal(name);
-		   settings.value.should.be.equal(value);
-		   settings.created.should.be.greaterThan(0);
-		   settings.updated.should.be.greaterThan(0);
-		   next(null, settings.id);
-		 });
+         var name = "Setting " + randomstring.generate({ length: 3, charset: 'numeric' });
+         var value = randomstring.generate();
+             models.settings.create({
+           setting_category_id: category_id,
+           name: name,
+           value: value
+         }).then(function(settings) {
+           settings.id.should.be.greaterThan(0);
+           settings.setting_category_id.should.equal(category_id);
+           settings.name.should.be.equal(name);
+           settings.value.should.be.equal(value);
+           settings.created.should.be.greaterThan(0);
+           settings.updated.should.be.greaterThan(0);
+           next(null, settings.id);
+         });
       },
       function updatesettings(settings_id, next){
-		var name = "Updated " + randomstring.generate({ length: 3, charset: 'numeric' });
-		var value = "updated " + randomstring.generate();
-        models.settings.update(
-          {  
-			name: name,
-			value: value,
-		  }, 
-          { where: { id: settings_id }} 
-		).then(function(user) {   
-		  next(null, settings_id, name, value);
-		});
+        var name = "Updated " + randomstring.generate({ length: 3, charset: 'numeric' });
+        var value = "updated " + randomstring.generate();
+            models.settings.update(
+              {  
+          name: name,
+          value: value,
+          }, 
+              { where: { id: settings_id }} 
+        ).then(function(user) {   
+          next(null, settings_id, name, value);
+        });
       },
       function findsettings(settings_id, name, value, next){
-		models.settings.find({where : { id : settings_id }}).then(function(settings){
-		  settings.id.should.be.greaterThan(0);
-		  settings.setting_category_id.should.be.greaterThan(0);
-		  settings.name.should.be.equal(name);
-		  settings.value.should.be.equal(value);
-		  settings.created.should.be.greaterThan(0);
-		  settings.updated.should.be.greaterThan(0);
-		  done();
-		});
+        models.settings.find({where : { id : settings_id }}).then(function(settings){
+          settings.id.should.be.greaterThan(0);
+          settings.setting_category_id.should.be.greaterThan(0);
+          settings.name.should.be.equal(name);
+          settings.value.should.be.equal(value);
+          settings.created.should.be.greaterThan(0);
+          settings.updated.should.be.greaterThan(0);
+          done();
+        });
+      }
+    ],function(error, result){
+     done();
+    });    
+  });
+});
+
+
+describe('delete settings', function () {
+  it('should delete a settings record', function (done) {
+     async.waterfall([
+       function createSettingCategory(next){
+          var name = randomstring.generate() + "-setting";
+          models.setting_categories.create({      
+            name: name
+          }).then(function(setting_categories) {
+            setting_categories.id.should.be.greaterThan(0);
+            setting_categories.name.should.equal(name);
+            setting_categories.created.should.be.greaterThan(0);
+            setting_categories.updated.should.be.greaterThan(0);
+            next(null, setting_categories.id);
+          });  
+       },
+       function createSetting(category_id, next){
+         var name = "Setting " + randomstring.generate({ length: 3, charset: 'numeric' });
+         var value = randomstring.generate();
+             models.settings.create({
+           setting_category_id: category_id,
+           name: name,
+           value: value
+         }).then(function(settings) {
+           settings.id.should.be.greaterThan(0);
+           settings.setting_category_id.should.equal(category_id);
+           settings.name.should.be.equal(name);
+           settings.value.should.be.equal(value);
+           settings.created.should.be.greaterThan(0);
+           settings.updated.should.be.greaterThan(0);
+           next(null, settings.id);
+         });
+      },
+      function deletesettings(settings_id){
+         models.settings.destroy({ where : { id : settings_id} })
+          .then(function(status) {
+	          models.settings.findAll({where : { id : settings_id }}).then(function(settingss){
+            settingss.length.should.equal(0);
+	          done();
+	        });
+        });
       }
     ],function(error, result){
      done();
