04f88dd63359fcb88e36eb4de5c25a92f80da8dc
yogesh-imorsetech <yogesh@imorsetech.com>
2016-08-01 18:28:05 +0530

    adding cases for country iso

--- test/functional/destinations.js
+++ test/functional/destinations.js
@@ -58,3 +58,76 @@ describe('GET /api/destinations?offset=2&limit=5', function(){
       });
   })
 });
+
+
+describe('GET /api/destinations/country/us', function(){
+  before(function(done) {
+    this.timeout(15000);
+
+    testUtils.ensureDestinationCount(12, function(err, result){
+      if(err){
+        should.fail();
+      }
+      else{
+        done();
+      }
+    });
+  });
+  it('should return destinations', function(done){
+    request(app)
+      .get('/api/destinations/country/us')
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .end(function(err, res) {
+      console.log('COUNT::'+ res.body.rows.length);
+        if (err) return done(err);
+        
+        res.body.count.should.be.above(5);
+        
+        // validate that country data is present
+        res.body.rows[0].country.iso_code2.should.be.equal('US');
+        res.body.rows[0].country.id.should.be.greaterThan(0);
+        res.body.rows[0].country.iso_code2.should.not.be.null();
+        res.body.rows[0].country.fips.should.not.be.null();
+        res.body.rows[0].country.name.should.not.be.null();
+
+        done();
+      });
+  })
+});
+
+describe('GET /api/destinations/country/us/?limit=10&offset=0', function(){
+  before(function(done) {
+    this.timeout(15000);
+
+    testUtils.ensureDestinationCount(10, function(err, result){
+      if(err){
+        should.fail();
+      }
+      else{
+        done();
+      }
+    });
+  });
+  it('should return 10 destinations from US', function(done){
+    request(app)
+      .get('/api/destinations/country/us/?limit=10&offset=0')
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .end(function(err, res) {
+        if (err) return done(err);
+        
+        res.body.count.should.be.above(10);
+        res.body.rows.length.should.be.equal(10);
+   
+        // validate that country data is present
+        res.body.rows[0].country.id.should.be.greaterThan(0);
+        res.body.rows[0].country.iso_code2.should.not.be.null();
+        res.body.rows[0].country.iso_code2.should.be.equal('US');
+        res.body.rows[0].country.fips.should.not.be.null();
+        res.body.rows[0].country.name.should.not.be.null();
+
+        done();
+      });
+  })
+});
