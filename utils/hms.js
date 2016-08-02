158dbeb1f1a32066a5357805a0714b716e5d5f56
rmcfadden <ryanpmcfadden@gmail.com>
2016-08-01 20:19:58 -0700

    cli users-provider

--- utils/hms.js
+++ utils/hms.js
@@ -1,13 +1,63 @@
 #!/usr/bin/env node
 'use strict';
 
-var testUtils  = require('../modules/test-utils');
+var program = require('commander');
 
+console.log('Node Environment =' + process.env.NODE_ENV);
 
-console.log('Node Environment =' + process.env.NODE_ENV)
+program
+  .command('add-user')
+  .option('-u, --username', 'Username [required]')
+  .option('-e, --email', 'Email [required]')
+  .option('-p, --password', 'Password [required]')
+  .option('-r, --roles', 'Roles')
+  .option('-o, --organizations', 'Organizations')
+  .action(function() {
+    var usersProvider  = require('../modules/users-provider');
+    var usersProv = new usersProvider();    
+    
+    console.log(program);
 
-testUtils.addTestDestinations(10, function(error){
+    if (!cmd.username){
+      argumentError('username');
+    }
+
+    if (!program.email){
+      argumentError('email');
+    }
+
+    if (!program.password){
+      argumentError('password');
+    }
+
+    var user = {
+      username: program.username,
+      email: program.email,
+      password: program.password,
+    }
+
+    usersProv.create(user, function(newUser){
+      process.exit();
+    });
+   });
+
+program
+  .command('add-test-data')  
+  .action(function(cmd) {
+    var testUtils  = require('../modules/test-utils');
+    testUtils.addTestDestinations(10, function(error){
+      process.exit();
+    });
+   });
+
+program.parse(process.argv);
+
+function argumentError(argName){
+  console.log();
+  console.log('ERROR: --' + argName + ' required!!');
+  program.help();
   process.exit();
-});
+}
 
 
+ 
