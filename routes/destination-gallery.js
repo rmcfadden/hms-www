db89fdb3d9bad45eff140183d0c00bf875d9e379
rmcfadden <ryanpmcfadden@gmail.com>
2016-08-14 11:37:30 -0700

    Destination view and started galleries

--- routes/destination-gallery.js
+++ routes/destination-gallery.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var assets = require('../modules/express-assets');
+
 
 var destinationsProvider = require('../modules/destinations-provider');
 var destinationsProv = new destinationsProvider();
 
-router.get('/destination-gallery/:iso_code2/:name', function(req, res, next) {
-  destinationsProv.findOneByIsoCode2AndName(req.params.iso_code2, req.params.name).then(function(destination){    
-    if(destination){
+router.get('/destination-gallery/:iso_code2/:name', 
 
-      res.render('destination-gallery', { destination : destination, scripts : [
-          '/assets/js/views/destination-gallery.js?app_version=' + res.app.locals.app_version
-          ] 
-      });
-    }
-  });
-});
+  assets.scripts(['/assets/js/plugins/imagesloaded.pkgd.js', 
+    '/assets/plugins/masonry/jquery.masonry.min.js',
+    '/assets/js/views/destination-gallery.js'] ), 
+  assets.stylesheets(['/assets/css/views/destination-gallery.css'] ), 
+
+  function(req, res, next) {
+    destinationsProv.findOneByIsoCode2AndName(req.params.iso_code2, req.params.name).then(function(destination){    
+      if(destination){
+        res.render('destination-gallery', { destination : destination});
+      }
+    });
+  }
+);
 
 module.exports = router;
--- routes/destination.js
+++ routes/destination.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
 var assets = require('../modules/express-assets');
-
 
 var destinationsProv = new (require('../modules/destinations-provider'));
 
 router.get('/destination/:iso_code2/:name', 
-  assets().scripts(['/assets/js/views/destination.js'] ), 
-  function(req, res, next) {
+  assets.scripts(['/assets/js/plugins/imagesloaded.pkgd.js', 
+      '/assets/plugins/masonry/jquery.masonry.min.js',
+      '/assets/js/plugins/animatedModal.min.js',
+      '/assets/js/views/destination.js'] ),
+  assets.stylesheets(['/assets/css/views/destination-gallery.css'] ),  
+    function(req, res, next) {
   destinationsProv.findOneByIsoCode2AndName(req.params.iso_code2, req.params.name).then(function(destination){    
     if(destination){
       res.render('destination', { destination : destination});
     }else{
       // TODO: note found
     }
-  })
+  });
 });
 
 module.exports = router;
