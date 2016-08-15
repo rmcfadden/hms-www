38476cac5b4111a37bd42b4a1c09b7a0005fccc7
rmcfadden <ryanpmcfadden@gmail.com>
2016-08-14 19:49:31 -0700

    destination views

--- public/assets/js/views/destination.js
+++ public/assets/js/views/destination.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-  var url = "https://maps.googleapis.com/maps/api/staticmap?center=" + "123+Chapala+St.+Santa+Barbara+CA+93101" + '&size=425x160&maptype=roadmap&scale=2'
+  var url = "https://maps.googleapis.com/maps/api/staticmap?center=" + "123+Chapala+St.+Santa+Barbara+CA+93101" + '&size=350x160&maptype=roadmap&scale=2'
   url += '&key=AIzaSyA9WxXXYFQ1M7X798jOcqKM79JMUrvuN5I';
 
   $('#image-map').attr("src", url);
